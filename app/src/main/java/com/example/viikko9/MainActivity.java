427d30cb06a6fc5d333eb92851babaaa3ad549a9
IiroVa <iiro.vahakangas@student.lut.fi>
2024-03-18 22:45:18 +0200

    v.6.0

--- app/src/main/java/com/example/viikko9/MainActivity.java
+++ app/src/main/java/com/example/viikko9/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.viikko9;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnLoad;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TEST
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            context = this;
+            UserStorage.getInstance().loadUsers(context);
             return insets;
         });
     }
@@ -28,11 +34,15 @@ public class MainActivity extends AppCompatActivity {
     public void switchtoAddUser(View view){
         Intent intent = new Intent(this,AddUserActivity.class);
         startActivity(intent);
+
     }
 
     public void switchtoListUser(View view){
         Intent intent = new Intent(this,ListUserInRecycleViewActivity.class);
         startActivity(intent);
     }
+
+
+
 
 }
